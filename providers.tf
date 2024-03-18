0573162e503540b11c4cf3266bde1a0915883114
“Murat <mnesiren14@gmail.com>
2024-03-18 18:35:37 -0500

    uncommented on providers

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-2"  
+  region = "us-east-2"
 }
 
 terraform {
   backend "s3" {
-    bucket                  = "terraform-s3-state"
-    key                     = "my-terraform-project"
-    #shared_credentials_file = "~/.aws/credentials"
+    bucket = "terraform-s3-state"
+    key    = "my-terraform-project"
+    shared_credentials_file = "~/.aws/credentials"
   }
 }
