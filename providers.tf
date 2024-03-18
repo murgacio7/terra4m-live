b6a572906d5e2f7dea69774b251516b245d6477d
“Murat <mnesiren14@gmail.com>
2024-03-18 17:43:43 -0500

    removed one region line

--- providers.tf
+++ providers.tf
@@ -6,7 +6,6 @@ terraform {
   backend "s3" {
     bucket                  = "terraform-s3-state"
     key                     = "my-terraform-project"
-    region                  = "us-east-2"
     #shared_credentials_file = "~/.aws/credentials"
   }
 }
