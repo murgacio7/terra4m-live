14cf1d59a56e650e01ef81fbb0e514f243a236ce
“Murat <mnesiren14@gmail.com>
2024-03-18 19:01:33 -0500

    changed main file

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "us-east-2"
 }
-
-terraform {
-  backend "s3" {
-    bucket                  = "terraform-s3-state"
-    key                     = "my-terraform-project"
-    shared_credentials_file = "~/.aws/credentials"
-  }
-}
-
