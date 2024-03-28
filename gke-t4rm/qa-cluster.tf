993efd2e27ab35170e902457c7dcf7f850099d19
“Murat <mnesiren14@gmail.com>
2024-03-28 13:00:58 -0500

    changed main files

--- gke-t4rm/qa-cluster.tf
+++ gke-t4rm/qa-cluster.tf
@@ -4,7 +4,7 @@ module "qa_gke_cluster1" {
   sa_display_name  = "QA service account for gke cluster"
   cluster_name     = "qa-cluster2"
   cluster_location = "us-central1-a"
+  region           = "us-central1"
   node_count       = 1
   disk_size_gb     = 60
-  region           = "us-central1"
 }
